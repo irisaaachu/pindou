d66797d7924f113d861fc4a2a06f825bd7c65cdf
irisaaachu <zhussssiiii@gmail.com>
2026-08-29 11:07:26 +0800

    feat: add Hello World landing page

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,23 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+  <view class="page">
+    <text class="title">Hello World</text>
   </view>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
-</script>
+<script setup lang="ts"></script>
 
-<style>
-.content {
+<style scoped>
+.page {
+  min-height: 100vh;
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
+  background-color: #ffffff;
 }
 
 .title {
-  font-size: 36rpx;
-  color: #8f8f94;
+  color: #1f2937;
+  font-size: 48rpx;
+  font-weight: 600;
 }
 </style>
